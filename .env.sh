a64375936002b2c258947dae0bb0c6f1f9c98a00
KyalSinLinLett <alexsin0012@gmail.com>
2023-03-25 22:42:06 +0630

    update mongodb link

--- .env.sh
+++ .env.sh
@@ -2,4 +2,4 @@
 
 export PORT="5000"
 export JWT_SECRET_KEY="qwertyuiop"
-export MONGO_URL="mongodb://localhost:27017/ewsd_db"
+export MONGO_URL="mongodb+srv://ewsd_admin:OScNmHBFPCr37Hew@ewsd.jeuk2v6.mongodb.net/ewsd_db"
