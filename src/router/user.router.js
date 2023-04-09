68a5092fdc3b729309bfef1e00468494b0aeb014
Wathone <96260192+Felip20@users.noreply.github.com>
2023-04-10 03:58:26 +0630

    updating route (#3)
    Co-authored-by: KyalSinLinLett <52605414+KyalSinLinLett@users.noreply.github.com>

--- src/router/user.router.js
+++ src/router/user.router.js
@@ -9,6 +9,8 @@ router.post("/get-user-count", authentication, UserController.getUserCount);
 router.post("/get-user-profile", authentication, UserController.getUserProfile);
 router.post("/get-all-user", authentication, UserController.getAllUser);
 router.post("/get-qa-coor", authentication, UserController.getQACoordinator);
+router.post("/get-qa-manager", authentication, UserController.getQAManager);
+router.post("/get-staff", authentication, UserController.getStaff);
 router.post("/edit-user", authentication, UserController.editUser);
 router.post("/delete-user", authentication, UserController.deleteUser);
 
