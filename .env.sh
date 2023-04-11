11697510f1232eca5933cc65858dad2e2f01a101
KyalSinLinLett <alexsin0012@gmail.com>
2023-04-11 07:37:03 +0630

    ready for submission

--- .env.sh
+++ .env.sh
@@ -2,5 +2,5 @@
 
 export PORT="5000"
 export JWT_SECRET_KEY="qwertyuiop"
-export MONGO_URL="mongodb+srv://ewsd_admin:OScNmHBFPCr37Hew@ewsd.jeuk2v6.mongodb.net/ewsd_db"
-# export MONGO_URL="mongodb://localhost:27017/ewsd_db"
+# export MONGO_URL="mongodb+srv://ewsd_admin:OScNmHBFPCr37Hew@ewsd.jeuk2v6.mongodb.net/ewsd_db"
+export MONGO_URL="mongodb://localhost:27017/ewsd_db"
